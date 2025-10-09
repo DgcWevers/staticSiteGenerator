815f79000fedc60172cfd3a2d52feaa346d18d5b
DgcWevers <danielle.wevers@protonmail.com>
2025-10-09 21:52:15 +0200

    last changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,37 @@
 from textnode import TextNode # type: ignore
+import logging, os, shutil, stat
+
 
 def main():
-    textnode = TextNode(text="This is some anchor text", text_type="link", url="https://www.boot.dev")
-    print(textnode)
+    logging.basicConfig(filename="logging.log",encoding='utf-8',level=logging.DEBUG)
+    copy_contents('public', 'static')
+
+
+def copy_contents(destination, source):
+    if not os.path.exists(destination):
+        logging.debug('destination folder does not exist')
+        os.mkdir(destination)
+    logging.debug(os.listdir(destination))
+    if len(os.listdir(destination)) > 0:
+        logging.debug(f"removing files in destination folder: {os.listdir(destination)}")
+        shutil.rmtree(destination)
+        os.mkdir(destination)
+    for i in os.listdir(source):
+        try:
+            logging.debug(f"coppying file/folder to new destination: {os.path.join(source, i)}")
+            if os.path.isfile(os.path.join(source, i)):
+                shutil.copy(os.path.join(source, i), os.path.join(destination, i))
+            else:
+                shutil.copytree(os.path.join(source, i), os.path.join(destination, i))
+        except PermissionError:
+            os.chmod(os.path.join(source, i), stat.S_IRWXO)
+            logging.debug(f"coppying file/folder to new destination: {os.path.join(source, i)}")
+            if os.path.isfile(os.path.join(source, i)):
+                shutil.copy(os.path.join(source, i), os.path.join(destination, i))
+            else:
+                shutil.copytree(os.path.join(source, i), os.path.join(destination, i))
+
 
 if __name__ == "__main__":
     main()
+
